8abe4600825962b0668e256e9fc46644953baed4
NicholasChristopherBlake <110085788+NicholasChristopherBlake@users.noreply.github.com>
2024-02-11 01:10:16 +0600

    fixed css styles

--- config/build/loaders/buildCssLoader.ts
+++ config/build/loaders/buildCssLoader.ts
@@ -4,12 +4,23 @@ import { RuleSetRule } from "webpack"
 export const buildCssLoader = (): RuleSetRule => {
   return {
     test: /\.css$/i,
-    use: [MiniCssExtractPlugin.loader, 
-    {
-      loader: 'css-loader', 
-      options: {
-        modules: true
-      },}
-    ]
+    use: [
+      {
+        // loader: 'style-loader',
+        loader: MiniCssExtractPlugin.loader,
+      }
+      , 
+      {
+        loader: 'css-loader', 
+        options: {
+          modules: {
+            auto: true,
+            localIdentName: "[path][name]__[local]--[hash:base64:5]",
+          },
+        },
+      }
+      ],
+      
   }
+  
 }
