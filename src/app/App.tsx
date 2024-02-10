522bc8a51de038ff727b2d765413324792be2689
NicholasChristopherBlake <110085788+NicholasChristopherBlake@users.noreply.github.com>
2024-02-11 01:17:20 +0600

    added css modules

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,10 @@
+import MainPage from "../pages/MainPage/ui/MainPage"
+
 export const App = () => {
   return (
   <div className="app">
     <h1>Hello there</h1>
+    <MainPage />
   </div>
   )
 }
---
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -0,0 +1,9 @@
+import cls from './MainPage.module.css';
+
+const MainPage = () => {
+  return (
+    <div className={cls.mainPage}>MainPage</div>
+  )
+}
+
+export default MainPage
